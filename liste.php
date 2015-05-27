959692f5cad2b8d0f80ace195793e9dcc21532fc
Cem Baykam <cbaykam@gmail.com>
2015-05-27 14:08:15 +0300

    Fix liste.php file indentation and rules

--- liste.php
+++ liste.php
@@ -1,47 +1,33 @@
 <?php
+	$urunadi = $_POST['urunadi'];
+	$urunadet = $_POST['adet'];
+	$urunfiyat = $_POST['fiyat'];
 
+	$test = array($urunadet, $urunfiyat);
+	if (is_numeric($urunadet)) {
+		echo "Adet sayi olmali."."\n";
+	}
 
-$urunadi = $_POST[urunadi];
-$urunadet = $_POST[adet];
-$urunfiyat = $_POST[fiyat];
+	if (!is_numeric($urunfiyat)) {
+		echo "Fiyat sayi olmali."."\n";
+	}
 
+	$total = $urunadet * $urunfiyat;
 
-$test = array($urunadet,$urunfiyat);
+	$fatura =  "urun adi " . $urunadi . " urun adeti " . $urunadet .
+		" urun fiyati " . $urunfiyat . " tutari " . $total . "\n" ;
 
+	if (!empty($urunadet) and !empty($urunadi) and !empty($urunfiyat)){	
+		if (is_numeric($urunadet) and is_numeric($urunadet)){
+			$file = file_put_contents('liste.txt',$fatura);
+		}
+	}else {
+		echo "bos alan birakmayin.";
+	}	
 
-	if (is_numeric($urunadet)) {}
-else {echo "Adet sayi olmali."."\n"; }
-
-if (is_numeric($urunfiyat)) {}
-	else {echo "Fiyat sayi olmali."."\n"; }
-
-$total = $urunadet *$urunfiyat;
-
-
-$fatura =  "urun adi"." ". $urunadi." urun adeti ".$urunadet.
-" urun fiyati ".$urunfiyat." tutari ". $total."\n" ;
-
-
-
-if (!empty($urunadet) and !empty($urunadi) and !empty($urunfiyat))
-{
-	
-	if (is_numeric($urunadet) and is_numeric($urunadet)) 
-	{
-		
-		$file = file_put_contents('liste.txt',$fatura);
-	};
-}
-
-else {
-	echo "bos alan birakmayin.";
-	
-}
-
-
-if($file) print "fatura eklendi.";
-else print "fatura eklenmedi.";
-
-
-		
+	if($file){
+		print "fatura eklendi.";
+	} else {
+		print "fatura eklenmedi.";	
+	}
 ?>
