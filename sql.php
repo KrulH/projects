f0409d0cc634e30af50f3cc200f6967a3b49f4a3
KrulH <mehmetcelik4@gmail.com>
2015-05-28 17:36:48 +0300

    sql yeni

--- sql.php
+++ sql.php
@@ -1,14 +1,31 @@
 <?php
-  $servername = "localhost";
-  $username = "root";
-  $password = "5750";
-
-  // Create connection
-  $conn = new mysqli($servername, $username, $password);
-
-  // Check connection
-  if ($conn->connect_error) {
+$servername = "localhost";
+$username = "root";
+$password = "5750";
+$dbname = "DATA";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-  } 
-  echo "Connected successfully";
+} 
+echo "Connected successfully";
+
+// $sql = "CREATE TABLE kayit (
+// id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+// firstname VARCHAR(30) NOT NULL,
+// lastname VARCHAR(30) NOT NULL
+// )";
+
+// if ($conn->query($sql) === TRUE) {
+// 	echo "Table MyGuests created successfully";
+// } else {
+// 	echo "Error creating table: " . $conn->error;
+// }
+
+
+
+
+
+
 ?>
