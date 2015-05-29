118704e0dd53866a177d62445382949ae4cfb087
KrulH <mehmetcelik4@gmail.com>
2015-05-29 10:44:23 +0300

    kayit liste çalıştı

--- kayit.php
+++ kayit.php
@@ -16,6 +16,21 @@ if ($conn->query($kaydet) === TRUE)
  { ECHO "KAYIT edildi";}
  else { echo "error";}
  
+ 
+ $sql = "SELECT id, firstname, lastname FROM kayit";
+ $result = $conn->query($sql);
+ 
+ if ($result->num_rows > 0) {
+ 	// output data of each row
+ 	while($row = $result->fetch_assoc()) {
+ 		echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+ 	}
+ } else {
+ 	echo "0 results";
+ }
+ $conn->close();
+ ?>
+  
 
 
 
