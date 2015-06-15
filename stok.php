e7b49a020d546db5ec514ab84786057ec0697799
KrulH <mehmetcelik4@gmail.com>
2015-06-15 10:28:01 +0300

    fatura total ekle

--- stok.php
+++ stok.php
@@ -1,5 +1,9 @@
 <?php
 
+
+
+
+
   include 'sql.php';
   
   
@@ -15,11 +19,16 @@
   	while($row = $result->fetch_assoc()) {
   		echo "fatura no:". $row["i_id"]." " ."Musteri Unvani:". $row["unvan"].
   		 " " ."Musteri adresi:". $row["Address"]. " "."aldigi urun adi:". $row["urun_ad"].
-  		 " "."urun adedi:". $row["urun_adet"]." " ."urun fiyati:". $row["urun_fiyat"]. " "."urun tutari:".$row["urun_adet"]*$row["urun_fiyat"]. "<br>"  
+  		 " "."urun adedi:". $row["urun_adet"]." " ."urun fiyati:"
+  		 		. $row["urun_fiyat"]. " "."urun tutari:".$row["urun_adet"]*$row["urun_fiyat"]. "<br>"  
   				 ;
   	 }
-
   	 
-  	
+  	$sqla = "select i_id,sum(urun_adet*urun_fiyat) as toplam from tablo group by i_id";
+  
+  	$sonuc = $conn->query($sqla);
+  	while($satir = $sonuc->fetch_assoc()) {
+  		echo "Fatura No:".$satir['i_id']." toplam: ".$satir['toplam']." TL" ."<br>"   ;
+  	}
   	 
 ?>
